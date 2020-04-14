802a380ecbea56b7d1d43d24e57405c5054a415d
Harsh Zalavadiya <harsh@foobars.in>
2020-04-15 03:35:49 +0530

    :tada: Initial Code

---
+++ stories/idb.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import IndexedDBProvider from "../src";
+import { IndexedDBConfig } from "../src/interfaces";
+import IDBOperations from "./idb-operations";
+
+export default {
+  title: "Default",
+};
+
+const idbConfig: IndexedDBConfig = {
+  databaseName: "fruity-db",
+  version: 1,
+  stores: [
+    {
+      name: "fruits",
+      id: { keyPath: "id", autoIncrement: true },
+      indices: [
+        { name: "name", keyPath: "name", options: { unique: false } },
+        { name: "qty", keyPath: "qty" },
+      ],
+    },
+  ],
+};
+
+export const Default = () => (
+  <IndexedDBProvider config={idbConfig}>
+    <IDBOperations />
+  </IndexedDBProvider>
+);
